23649d3adc2b9f3977a777cd05910160bcfff7d5
Nitin Ticroliya <ticroliya2309@gmail.com>
2022-04-16 12:08:54 +0530

    my portfolio website

---
+++ index.js
@@ -0,0 +1,25 @@
+  // var, let, const
+  var a=1;
+  var b="this is a string";
+  var c= 1.34
+  console.log(b.length);
+
+  // array
+
+  let arr=[1,2,3,4,5];
+  arr.push(6);
+  console.log(arr);
+
+  // object
+
+  let obj = {
+      name: "Nitin Ticroliya",
+      Age: 20
+  };
+  console.log(obj);
+  console.log(obj.name)
+
+  let str= "I am nitin ticroliya";
+  class Animal {
+    
+  }
